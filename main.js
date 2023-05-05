7f64667e55804dd366fcd998628adbf11472c8aa
ThatFunnyWeirdo <112593486+ThatFunnyWeirdo@users.noreply.github.com>
2023-05-05 15:59:48 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 480);
+    canvas.position(110, 250);
+}
+
+function draw()
+{
+
+}
+
+function save()
+{
+    save("filtered_image.png")
+}
